7e4ccfaf1a726c4287c3d19cd5fc9d7c3343d359
lampochka112 <daniilgolosov232@gmail.com>
2024-11-08 17:01:27 +0000

    done № 3

---
+++ task3.py
@@ -0,0 +1,9 @@
+number = int(input("Введите число:"))
+if number > 0: 
+    print("число больше нуля")
+elif number == 0:
+    print("число рвыно нулю")
+else:
+    print("число меньше нуля")
+     
+     
