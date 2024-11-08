571d158b8e130ef0cdb6ae80473a74938cebf225
lampochka112 <daniilgolosov232@gmail.com>
2024-11-08 17:17:35 +0000

    done tassk4

---
+++ task4.py
@@ -0,0 +1,9 @@
+number1 = int(input("введите первое число:"))
+number2 = int(input("введите второе число:"))
+if number1 == number2:
+    print("числа равны")
+elif number1 < number2
+    print(number1, number2)
+else:
+    print(number1, number2)
+    
