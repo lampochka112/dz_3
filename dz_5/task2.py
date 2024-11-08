e0b368b2804244da35e05b7c9c86efb55153f7ac
lampochka112 <daniilgolosov232@gmail.com>
2024-11-08 18:01:25 +0000

    done task2

---
+++ dz_5/task2.py
@@ -0,0 +1,6 @@
+number = int(input("Введите любое число:"))
+degree = int(input("введите втрое число:"))
+if degree >= 0 and degree <8:
+    print (number ** degree)
+else:
+    print("Неправильная степень") 
